a6fbe8d2a977383ab95e05b8d25c24defbd96194
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-07-12 16:31:01 +0530

    New code has been added

---
+++ SampleMaven_Framework/src/test/java/com/crm/vtiger/organization/CreateOrgTest.java
@@ -0,0 +1,30 @@
+package com.crm.vtiger.organization;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrgTest() 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost:8888/");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("manager", Keys.ENTER);
+		
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+	}
+
+}
